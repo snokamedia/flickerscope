b02828fb73c6472f9b2565e13425ea2db2b3b2a3
Snoka Media <info@snoka.ca>
2026-07-11 12:46:12 -0600

    add version 0.3.1-beta to footer

--- src/components/PrivacyNotice.tsx
+++ src/components/PrivacyNotice.tsx
@@ -1,6 +1,9 @@
+import pkg from '../../package.json';
+
 export function PrivacyNotice() {
   return (
-    <div className="mt-8 flex flex-col items-center gap-3 text-center text-[11px] text-text-dim">
+    <div className="mt-8 flex flex-col items-center gap-2 text-center text-[11px] text-text-dim">
+      <span>v{pkg.version}</span>
       <div className="flex items-center gap-3">
         <a className="github-button" href="https://github.com/snokamedia/flickerscope" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star snokamedia/flickerscope on GitHub">Star</a>
         <a className="github-button" href="https://github.com/snokamedia" data-icon="octicon-follow" data-size="large" data-show-count="true" aria-label="Follow @snokamedia on GitHub">Follow @snokamedia</a>
